880cf1dc212495b2aa0e71ef3df60032aeb55ef6
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-20 20:54:49 -0400

    Created API Backend

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,47 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+import requests
 
 chatbot = ChatBot("FirstChatBot")
 
-chatbotConversation = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
+chatbotConversation = []
+
+def adviceInfo():
+    try:
+        adviceAPI = requests.get("https://api.adviceslip.com/advice")
+    except:
+        print("")  
+        
+    advice = adviceAPI.json()["slip"]
+    advice = advice["advice"]
+    chatbotConversation.append(advice)
+    
+def dogInfo():
+    try:
+        dogAPI = requests.get("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1")
+    except:
+        print("")  
+        
+    dog = dogAPI.json()["fact"]
+    chatbotConversation.append(dog)
+
+print("Loading Chatbot...")
+while (len(chatbotConversation) <= 100): #this controls the length of the list
+    adviceInfo()
+    
 
 trainer = ListTrainer(chatbot)
 
 trainer.train(chatbotConversation)
 
+def responsePicker(usr):
+    if (len(usr) <= 15):
+        print("ChatBot: " + str(chatbot.get_response(f"{usr}")))
+    elif (len(usr) >= 20):
+        print("ChatBot: " + str(chatbot.get_response(f"{chatbotConversation}")))
+
+
 while True:
 
-    usr = input("What would you like to say... ")
-    response = chatbot.get_response(f"{usr}")
-    print(f"ChatBot: {response}")
+    usr = input("What advice would you like... ")
+    print(f"{responsePicker(usr)}")
