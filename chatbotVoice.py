b8a80354d72ed4392ad070cd9ca532b649294a58
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-04-01 15:39:23 -0400

    basic chatbot response

--- chatbotVoice.py
+++ chatbotVoice.py
@@ -3,3 +3,21 @@ from chatterbot.trainers import ListTrainer
 from playsound import playsound
 import requests
 import gtts
+
+chatbot = ChatBot("Gracie")
+
+testConversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(testConversation)
+
+response = chatbot.get_response("how is your day")
+print(f"Gracie: {response}")
