6fb85195fb55140fce5e7cfc3c850852c73df935
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-04-05 21:28:57 -0400

    added custom corpus import

--- chatbotCustom-trainData.py
+++ chatbotCustom-trainData.py
@@ -3,3 +3,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 chatbot = ChatBot("TrainData")
 trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train(
+    "corpusData/corupusTrain_test_2.json"
+)
