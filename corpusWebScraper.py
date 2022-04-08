bc4b3b9efe0f53acf75096263cf0d2b1777def0f
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-04-08 19:29:22 -0400

    Created --> dataChecker(scrapedData)
    this checks if any data was scraped from an url

--- corpusWebScraper.py
+++ corpusWebScraper.py
@@ -3,12 +3,22 @@ import json
 
 myScraper = ContentScraper()
 
+def dataChecker(scrapedData):
+    if (str(scrapedData) == "[]"):
+        print("failed to find related info")
+        return False
+    else:
+        return True
+    
+
 while True:
     usrURL = input("Paste a url: ")
     usrWants = [input(f"What would you like from {usrURL}? ")]
     
     myResult = myScraper.build(usrURL, usrWants)
+    dataChecker(myResult)
      
-    outputFile = open("corpusData/contentScraperDump.json", "a")
-    json.dump(myResult, outputFile) # adds as a list but will need json formatting
-    print("\n\ncontent added to json file\n\n")
+    if dataChecker(myResult) == True:
+        outputFile = open("corpusData/contentScraperDump.json", "a")
+        json.dump(myResult, outputFile) # adds as a list but will need json formatting
+        print("\n\ncontent added to json file\n\n")
