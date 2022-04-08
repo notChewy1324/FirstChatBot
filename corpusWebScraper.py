bca83fc733dcd720c9178b7ae6b6060dd5110626
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-04-07 22:34:25 -0400

    created/added the contentScraper custom import

--- corpusWebScraper.py
+++ corpusWebScraper.py
@@ -1,4 +1,7 @@
-from autoscraper import AutoScraper
+from autoscraper import AutoScraper # from a pip install / will remove when un-needed
+
+from contentScraper import ContentScraper # Will use this as the local custom backend code instead of 3rd party pip
+
 scraper = AutoScraper()
 
 url = 'corpusData/corupusTrain_test_2.json'
